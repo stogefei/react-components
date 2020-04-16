e37211e55062b701709da425fb96d15bc57392b2
zhuaofei <2353536563@qq.com>
2020-04-16 22:46:23 +0800

    添加基础颜色

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React, {useState} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './styles/index.scss';
 import LikeButton from './components/button'
 import MouseTracker from './components/MouseTracker'
 import MouseOverTracker from './hooks/moseOverTracker'
@@ -10,18 +9,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <h2>x:{postions.x}  y:{postions.y} </h2>
         <button onClick={() => {setShow(!show)}}> toggle show</button>
         <LikeButton></LikeButton>
--- src/hooks/moseOverTracker.tsx
+++ src/hooks/moseOverTracker.tsx
@@ -1,4 +1,4 @@
-import React, {useState ,useEffect} from 'react'
+import {useState ,useEffect} from 'react'
 const MouseOverTracker = () => {
     const [position, setPosition] = useState({x: 0, y: 0})
     useEffect(() =>{
